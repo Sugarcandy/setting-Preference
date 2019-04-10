37e9e4cb2d3a5c01b63df5aa5dd5c26a49def9de
Sugarcandy <1947291437@qq.com>
2019-04-10 13:33:40 +0800

    first

---
+++ app/src/main/java/com/example/setting/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.setting;
+
+import android.content.Intent;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fram, new SettingsFragment()).commit();
+    }
+
+
+}
+
